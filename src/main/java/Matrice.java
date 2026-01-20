8c281293200e919e5252eadb680df7c608de2bb4
artieur262 <raguinmateo@gmail.com>
2026-01-20 12:29:38 +0100

    correction de la correction

--- src/main/java/Matrice.java
+++ src/main/java/Matrice.java
@@ -59,6 +59,7 @@ public class Matrice {
         for (int j = 0; j < grid[0].length; j++) {
             System.out.print(" ---");
         }
+        System.out.println();
     }   
 
     public void print() {
@@ -68,7 +69,6 @@ public class Matrice {
         }
         for (int i = 0; i < grid.length; i++) {
             printRowSeparator();
-            System.out.println();
             System.out.print("| ");
             for (int j = 0; j < grid[i].length; j++) {
                 System.out.print(grid[i][j] + " | ");
